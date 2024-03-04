a34460119c8bf79b10fd0f271fb130ca312304dc
Abdulla Zin <abdullah.hzen@gmail.com>
2024-03-04 01:22:09 -0500

    Initial project commit

---
+++ Assets/CueController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CueController : MonoBehaviour
+{
+    public GameObject cueBall;
+    public GameObject triangle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void HitBall()
+    {
+        cueBall.GetComponent<Rigidbody>().AddForce(Vector3.forward * Time.deltaTime * 10, ForceMode.Impulse);
+    }
+    public void HideTriangle()
+    {
+        triangle.active = false;
+    }
+}
